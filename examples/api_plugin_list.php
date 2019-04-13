8973b2593e63716a5ef68691a0416ddce2ec08d1
Laurent Laville <laurent.laville@gmail.com>
2019-04-13 10:13:44 +0200

    fixed plugin list example

--- examples/api_plugin_list.php
+++ examples/api_plugin_list.php
@@ -9,23 +9,47 @@
  * @link     http://php5.laurent-laville.org/reflect/
  * @since    Example available since Release 3.0.0-alpha3+1
  */
-require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-use Bartlett\Reflect\Client;
-use Bartlett\Reflect\Environment;
+$loader = require_once dirname(__DIR__) . '/vendor/autoload.php';
+$loader->addClassMap(
+    [
+        'YourNamespace\LogPlugin'
+        =>  __DIR__ . '/YourNamespace/LogPlugin.php',
+        'YourNamespace\YourLogger'
+        =>  __DIR__ . '/YourNamespace/YourLogger.php',
+    ]
+);
 
-// defines environment where to find the JSON config file
-if (!getenv("BARTLETTRC")) {
-    putenv("BARTLETTRC=phpreflect.json");
-}
-Environment::setScanDir();
+use Bartlett\Reflect\Application\Command\PluginListCommand;
+use Bartlett\Reflect\Application\Command\PluginListHandler;
+use League\Tactician\CommandBus;
+use League\Tactician\Handler\CommandHandlerMiddleware;
+use League\Tactician\Handler\Locator\InMemoryLocator;
+use League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor;
+use League\Tactician\Handler\MethodNameInflector\InvokeInflector;
 
-// creates an instance of client
-$client = new Client();
+$locator = new InMemoryLocator();
+$locator->addHandler(
+    new PluginListHandler(),
+    PluginListCommand::class
+);
 
-// request for a Bartlett\Reflect\Api\Plugin
-$api = $client->api('plugin');
+$handlerMiddleware = new CommandHandlerMiddleware(
+    new ClassNameExtractor(),
+    $locator,
+    new InvokeInflector()
+);
 
-$plugins = $api->dir();
+$commandBus = new CommandBus([$handlerMiddleware]);
+
+$command = new PluginListCommand(
+    __DIR__ . '/../vendor',
+    __DIR__ . '/../',
+    __DIR__ . '/YourNamespace/yournamespace.json',
+    false
+);
+
+// equivalent to CLI command `phpreflect bartlett:plugin:list`
+$plugins = $commandBus->handle($command);
 
 print_r($plugins);

51b896cd334d6fcfc9dc63434c95762acc179f62
Laurent Laville <laurent.laville@gmail.com>
2019-04-13 09:26:28 +0200

    fixed logger plugin example

--- examples/YourNamespace/YourLogger.php
+++ examples/YourNamespace/YourLogger.php
@@ -6,8 +6,23 @@ use Psr\Log\AbstractLogger;
 
 class YourLogger extends AbstractLogger
 {
-    public function log($level, $message, array $context = array())
+    public function log($level, $message, array $context = [])
     {
-        printf('%s : %s%s', $level, $message, PHP_EOL);
+        printf('%s : %s%s', $level, $this->interpolate($message, $context), PHP_EOL);
+    }
+
+    private function interpolate($message, array $context = [])
+    {
+        // build a replacement array with braces around the context keys
+        $replace = [];
+        foreach ($context as $key => $val) {
+            // check that the value can be casted to string
+            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
+                $replace['{' . $key . '}'] = $val;
+            }
+        }
+
+        // interpolate replacement values into the message and return
+        return strtr($message, $replace);
     }
 }
--- examples/api_analyser_run_with_logger.php
+++ examples/api_analyser_run_with_logger.php
@@ -2,6 +2,41 @@
 /**
  * Examples of Structure analyser's run with a custom log plugin.
  *
+ * <code>
+ *   array (
+ *   'files' =>
+ *   array (
+ *     // ...
+ *   ),
+ *   'errors' =>
+ *   array (
+ *   ),
+ *   'Bartlett\\Reflect\\Application\\Analyser\\StructureAnalyser' =>
+ *   array (
+ *     'namespaces' => 20,
+ *     'interfaces' => 8,
+ *     'traits' => 0,
+ *     'classes' => 57,
+ *     'abstractClasses' => 7,
+ *     'concreteClasses' => 50,
+ *     'functions' => 8,
+ *     'namedFunctions' => 0,
+ *     'anonymousFunctions' => 8,
+ *     'methods' => 265,
+ *     'publicMethods' => 211,
+ *     'protectedMethods' => 53,
+ *     'privateMethods' => 1,
+ *     'nonStaticMethods' => 261,
+ *     'staticMethods' => 4,
+ *     'constants' => 0,
+ *     'classConstants' => 12,
+ *     'globalConstants' => 0,
+ *     'magicConstants' => 3,
+ *     'testClasses' => 0,
+ *     'testMethods' => 0,
+ *   ),
+ * </code>
+ *
  * @category PHP
  * @package  PHP_Reflect
  * @author   Laurent Laville <pear@laurent-laville.org>
@@ -12,33 +47,47 @@
 
 $loader = require_once dirname(__DIR__) . '/vendor/autoload.php';
 $loader->addClassMap(
-    array(
+    [
         'YourNamespace\LogPlugin'
             =>  __DIR__ . '/YourNamespace/LogPlugin.php',
         'YourNamespace\YourLogger'
             =>  __DIR__ . '/YourNamespace/YourLogger.php',
-    )
+    ]
 );
 
-use Bartlett\Reflect\Client;
+use Bartlett\Reflect\Application\Command\AnalyserRunCommand;
+use Bartlett\Reflect\Application\Command\AnalyserRunHandler;
+use League\Tactician\CommandBus;
+use League\Tactician\Handler\CommandHandlerMiddleware;
+use League\Tactician\Handler\Locator\InMemoryLocator;
+use League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor;
+use League\Tactician\Handler\MethodNameInflector\InvokeInflector;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
-// set our own location of JSON config file
-putenv("BARTLETT_SCAN_DIR=" . __DIR__ . '/YourNamespace');
+$locator = new InMemoryLocator();
+$locator->addHandler(
+    new AnalyserRunHandler(
+        new EventDispatcher(),
+        __DIR__ . '/YourNamespace/yournamespace.json'
+    ),
+    AnalyserRunCommand::class
+);
 
-// set our own JSON config file
-putenv("BARTLETTRC=yournamespace.json");
+$handlerMiddleware = new CommandHandlerMiddleware(
+    new ClassNameExtractor(),
+    $locator,
+    new InvokeInflector()
+);
 
-// creates an instance of client
-$client = new Client();
-
-// request for a Bartlett\Reflect\Api\Analyser
-$api = $client->api('analyser');
+$commandBus = new CommandBus([$handlerMiddleware]);
 
 // perform request, on a data source with default analyser (structure)
 $dataSource = dirname(__DIR__) . '/src';
-$analysers  = array('structure');
+$analysers  = ['structure'];
 
-// equivalent to CLI command `phpreflect analyser:run ../src`
-$metrics = $api->run($dataSource, $analysers);
+$command = new AnalyserRunCommand($dataSource, $analysers, false);
+
+// equivalent to CLI command `phpreflect bartlett:analyser:run ../src`
+$metrics = $commandBus->handle($command);
 
 var_export($metrics);

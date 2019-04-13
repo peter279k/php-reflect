47178ff05f00bbc1c7be5252c50e9b66647b7d38
Laurent Laville <laurent.laville@gmail.com>
2019-04-13 09:27:11 +0200

    fixed standard example plugin

--- examples/api_analyser_run.php
+++ examples/api_analyser_run.php
@@ -8,48 +8,52 @@
  *  array (
  *    // ...
  *  ),
- *  'Bartlett\\Reflect\\Analyser\\StructureAnalyser' =>
+ *
+ *  'errors' =>
  *  array (
- *    'namespaces' => 19,
- *    'interfaces' => 7,
+ *  ),
+ *  'Bartlett\\Reflect\\Application\\Analyser\\StructureAnalyser' =>
+ *  array (
+ *    'namespaces' => 20,
+ *    'interfaces' => 8,
  *    'traits' => 0,
- *    'classes' => 56,
- *    'abstractClasses' => 6,
+ *    'classes' => 57,
+ *    'abstractClasses' => 7,
  *    'concreteClasses' => 50,
- *    'functions' => 6,
+ *    'functions' => 8,
  *    'namedFunctions' => 0,
- *    'anonymousFunctions' => 6,
- *    'methods' => 280,
- *    'publicMethods' => 241,
- *    'protectedMethods' => 29,
- *    'privateMethods' => 10,
- *    'nonStaticMethods' => 273,
- *    'staticMethods' => 7,
+ *    'anonymousFunctions' => 8,
+ *    'methods' => 265,
+ *    'publicMethods' => 211,
+ *    'protectedMethods' => 53,
+ *    'privateMethods' => 1,
+ *    'nonStaticMethods' => 261,
+ *    'staticMethods' => 4,
  *    'constants' => 0,
- *    'classConstants' => 17,
+ *    'classConstants' => 12,
  *    'globalConstants' => 0,
  *    'magicConstants' => 3,
  *    'testClasses' => 0,
  *    'testMethods' => 0,
  *  ),
- *  'Bartlett\\Reflect\\Analyser\\LocAnalyser' =>
+ *  'Bartlett\\Reflect\\Application\\Analyser\\LocAnalyser' =>
  *  array (
- *    'llocClasses' => 995,
+ *    'llocClasses' => 988,
  *    'llocByNoc' => 0,
  *    'llocByNom' => 0,
- *    'llocFunctions' => 48,
+ *    'llocFunctions' => 34,
  *    'llocByNof' => 0,
  *    'llocGlobal' => 0,
- *    'classes' => 56,
- *    'functions' => 6,
- *    'methods' => 303,
- *    'cloc' => 117,
- *    'eloc' => 2700,
- *    'lloc' => 1043,
- *    'wloc' => 329,
- *    'loc' => 3146,
- *    'ccn' => 475,
- *    'ccnMethods' => 451,
+ *    'classes' => 57,
+ *    'functions' => 8,
+ *    'methods' => 293,
+ *    'cloc' => 79,
+ *    'eloc' => 2568,
+ *    'lloc' => 1022,
+ *    'wloc' => 311,
+ *    'loc' => 2958,
+ *    'ccn' => 469,
+ *    'ccnMethods' => 449,
  *  ),
  * )
  * </code>
@@ -64,19 +68,39 @@
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-use Bartlett\Reflect\Client;
+use Bartlett\Reflect\Application\Command\AnalyserRunCommand;
+use Bartlett\Reflect\Application\Command\AnalyserRunHandler;
+use League\Tactician\CommandBus;
+use League\Tactician\Handler\CommandHandlerMiddleware;
+use League\Tactician\Handler\Locator\InMemoryLocator;
+use League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor;
+use League\Tactician\Handler\MethodNameInflector\InvokeInflector;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
-// creates an instance of client
-$client = new Client();
+$locator = new InMemoryLocator();
+$locator->addHandler(
+    new AnalyserRunHandler(
+        new EventDispatcher(),
+        __DIR__ . '/../bin/phpreflect.json.dist'
+    ),
+    AnalyserRunCommand::class
+);
 
-// request for a Bartlett\Reflect\Api\Analyser
-$api = $client->api('analyser');
+$handlerMiddleware = new CommandHandlerMiddleware(
+    new ClassNameExtractor(),
+    $locator,
+    new InvokeInflector()
+);
+
+$commandBus = new CommandBus([$handlerMiddleware]);
 
 // perform request, on a data source with two analysers (structure, loc)
 $dataSource = dirname(__DIR__) . '/src';
-$analysers  = array('structure', 'loc');
+$analysers  = ['structure', 'loc'];
 
-// equivalent to CLI command `phpreflect analyser:run ../src structure loc`
-$metrics = $api->run($dataSource, $analysers);
+$command = new AnalyserRunCommand($dataSource, $analysers, true);
+
+// equivalent to CLI command `phpreflect bartlett:analyser:run ../src structure loc`
+$metrics = $commandBus->handle($command);
 
 var_export($metrics);
